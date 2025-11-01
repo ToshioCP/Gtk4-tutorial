4a25597e2ba01cd8f3bda22eb162e63d0ecf6b0c
Jamie Drinkell <94254558+M0JXD@users.noreply.github.com>
2025-11-01 00:39:09 +0000

    add space to pr2

--- src/misc/pr2.c
+++ src/misc/pr2.c
@@ -12,7 +12,7 @@ main (int argc, char **argv) {
 
   app = gtk_application_new ("com.github.ToshioCP.pr2", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
-  stat =g_application_run (G_APPLICATION (app), argc, argv);
+  stat = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
   return stat;
 }
